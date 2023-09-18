8ff26606a743b4f895559baec453f63831f65065
nejmiyassine <devtwins0@gmail.com>
2023-09-18 09:58:18 +0100

    Finishing the blog backend

--- api/controllers/blogController.js
+++ api/controllers/blogController.js
@@ -5,7 +5,7 @@ const Blog = require('../models/Blog');
 // Image Upload
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+        cb(null, 'public/uploads/');
     },
     filename: (req, file, cb) => {
         cb(null, file.fieldname + '_' + Date.now() + '_' + file.originalname);
